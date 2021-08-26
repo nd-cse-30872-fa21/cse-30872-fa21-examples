5a93adaf4d0397f933c016c1f1db34792fdaba52
Peter Bui <pbui@nd.edu>
2021-08-26 15:22:29 -0400

    Lecture 07: add Exercise 07-A

---
+++ lecture07/exercise07-A/solution.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+# Exercise-07-A: Perfect Valid Square
+
+import sys
+
+def is_perfect_square(n):
+    ''' Binary search (Iterative) '''
+    low, high = 1, n
+
+    while low <= high:
+        middle = (low + high) // 2
+        square = middle * middle
+        
+        if square == n:
+            return True
+
+        if square > n:
+            high = middle - 1
+        else:
+            low  = middle + 1
+
+    return False
+
+def is_perfect_square_r(low, high, n):
+    ''' Binary search (recursive) '''
+    if low > high:
+        return False
+
+    middle = (low + high) // 2
+    square = middle * middle
+        
+    if square == n:
+        return True
+
+    if square > n:
+        return is_perfect_square_r(low, middle - 1, n)
+    else:
+        return is_perfect_square_r(middle + 1, high, n)
+
+def is_perfect_square(n):
+    ''' Binary search (recursive) '''
+    return is_perfect_square_r(1, n, n)
+
+'''
+# Linear search
+def is_perfect_square(n):
+    for i in range(1, n):
+        if i*i == n:
+            return True
+    return False
+'''
+
+def main():
+    for n in map(int, sys.stdin):
+        print('Yes' if is_perfect_square(n) else 'No')
+
+if __name__ == '__main__':
+    main()
---
+++ lecture07/exercise07-A/template.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Exercise 07-A: Perfect Valid Square
+
+import sys
+
+def is_perfect_square(n):
+    # TODO: Use binary search to determine if n is a perfect valid square
+    return False
+
+def main():
+    for n in map(int, sys.stdin):
+        print('Yes' if is_perfect_square(n) else 'No')
+
+if __name__ == '__main__':
+    main()
