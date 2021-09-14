47c7bf2684be680dff5695000117edd94a30f1ff
Peter Bui <pbui@nd.edu>
2021-09-14 14:12:02 -0400

    Lecture 16: add Exercise 16-A

---
+++ lecture16/exercise16-A/solution.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Exericse 16-A: Climbing Stairs
+
+import sys
+
+# Functions
+
+def count_steps(n, cache={}):
+    if n <= 2:
+        return n
+
+    if n not in cache:
+        cache[n] = (count_steps(n - 1, cache) +
+                    count_steps(n - 2, cache))
+
+    return cache[n]
+
+def main():
+    for n in map(int, sys.stdin):
+        print(count_steps(n))
+
+# Main Execution
+
+if __name__ == '__main__':
+    main()
